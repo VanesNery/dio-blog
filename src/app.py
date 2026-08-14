83a3d7f9f08501ed46d1c280b47a90de50a00e30
Vanessa Nery <54214968+VanesNery@users.noreply.github.com>
2026-08-14 22:55:27 +0000

    refatorando o arquivo app, movendo codigo para models

--- src/app.py
+++ src/app.py
@@ -1,52 +1,11 @@
-from datetime import datetime
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-import sqlalchemy as sa
+from src.models import db
 
-
-class Base(DeclarativeBase):
-  pass
-
-db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 jwt = JWTManager()
-
-
-class Role(db.Model):
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(sa.String, nullable=False)
-    user: Mapped[list['User']] = relationship('User', back_populates='role')
-
-    def __repr__(self) -> str:
-        return f"Role(id={self.id!r}, name={self.name!r})"
-
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(sa.String, nullable=False)
-    active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
-    role_id: Mapped[int] = mapped_column(sa.ForeignKey('role.id'), nullable=True)
-    role: Mapped['Role'] = relationship('Role', back_populates='user')
-
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, username={self.username!r}, active={self.active!r})"
-
-
-class Post(db.Model):
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(sa.String, nullable=False)
-    body: Mapped[str] = mapped_column(sa.String, nullable=False)
-    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
-    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
 
 
 def create_app(test_config=None):
